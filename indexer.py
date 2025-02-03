d3347738028f1beec106819cccb3a0f4bf822056
catcarbonell <catcarbonell@gmail.com>
2025-02-03 14:57:57 -0800

    created separate module folders for indexer and scraper

--- indexer.py
+++
@@ -1,6 +0,0 @@
-from llama_index.core import SimpleDirectoryReader, GPTVectorStoreIndex
-
-def create_index():
-    documents = SimpleDirectoryReader("documents").load_data()
-    index = GPTVectorStoreIndex.from_documents(documents)
-    return index
